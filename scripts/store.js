8e78a3f978d2ef8be1830164ca9daf2712e9a4b7
Hubert Yang <offspring7890@yahoo.com>
2019-08-21 09:18:42 -0700

    finish drill #3

--- scripts/store.js
+++ scripts/store.js
@@ -7,7 +7,32 @@ const store = (function () {
           { id: cuid(), name: 'bread', checked: false }
         ],
         hideCheckedItems: false,
-        searchTerm: ''
+        searchTerm: '',
+        findById(id) {
+            return store.items.find(item => item.id === id);
+        },
+        addItem(name) {
+            try {
+                Item.validateName(name);
+                this.items.push(Item.create(name));
+            } catch (e) {
+
+            }
+        },
+        findAndToggleChecked(id) {
+            this.findById(id).checked = !this.findById(id).checked;
+        },
+        findAndUpdateName(id, newName) {
+            try {
+                Item.validateName(newName);
+                this.findById(id).name = newName;
+            } catch (e) {
+                console.log(`Cannot update name: ${e.message}`)
+            }
+        },
+        findAndDelete(id) {
+            this.items = this.items.filter(item => item.id !== id);
+        }
     };
     return store;
 }());
